f08a9ded69af8c0a40ff93285578c663d36f94fd
aryagaikwad <aryagaikwad@gmail.com>
2026-06-21 13:31:37 +0530

    Fix Render port

--- Desktop/InsightDoc-Ai/backend/app.py
+++ Desktop/InsightDoc-Ai/backend/app.py
@@ -40,7 +40,13 @@ def home():
     }
 
 
+import os
+
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
